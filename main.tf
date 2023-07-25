9f444d272a16d76153eb7dbe7cd49646583a768c
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-07-25 19:29:18 +0530

    ..

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
-#   tags = merge(
-#     local.common_tags,
-#     { Name = "${var.env}-vpc" }
-#   )
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-vpc" }
+  )
 }
 
 # resource "aws_vpc_peering_connection" "peer" {
