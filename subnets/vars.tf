a7f495de7d6ea1642b603410af9e13118391e82b
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-07-30 23:04:32 +0530

    ..

--- subnets/vars.tf
+++ subnets/vars.tf
@@ -1,18 +1,17 @@
 variable "cidr_block" {}
 variable "env" {}
-variable "cidr_block1" {} //r
-# variable "availability_zone" {}
-# variable "vpc_id" {}
-# variable "default_vpc_id" {}
-# variable "name" {}
-# variable "vpc_peering_connection_id" {}
-# variable "tags" {}
-# variable "internet_gw" {}
-# variable "nat_gw" {}
-#
-# variable "gateway_id" {
-#   default = null
-# }
-# variable "nat_gw_id" {
-#   default = null
-# }
+variable "availability_zone" {}
+variable "vpc_id" {}
+variable "default_vpc_id" {}
+variable "name" {}
+variable "vpc_peering_connection_id" {}
+variable "tags" {}
+variable "internet_gw" {}
+variable "nat_gw" {}
+
+variable "gateway_id" {
+  default = null
+}
+variable "nat_gw_id" {
+  default = null
+}
