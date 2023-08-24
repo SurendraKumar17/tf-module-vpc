ab989eca81a3e83301ee487ca78aa5d5f55561de
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-08-24 19:51:41 +0530

    ...

--- output.tf
+++ output.tf
@@ -6,10 +6,10 @@ output "vpc_peering_connection_id" {
   value = aws_vpc_peering_connection.peer.id
 }
 
-output "public_subnet_ids" {
-  value = module.public_subnets
-}
-
-output "private_subnet_ids" {
-  value = module.private_subnets
-}
+# output "public_subnet_ids" {
+#   value = module.public_subnets
+# }
+#
+# output "private_subnet_ids" {
+#   value = module.private_subnets
+# }
